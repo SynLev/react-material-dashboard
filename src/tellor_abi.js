69d36bbfcd0f53faba50935e6ab09e98430fa263
AnthonyPiazza <piazza.anthony.n@gmail.com>
2020-10-24 21:46:49 -0600

    Pruning

---
+++ src/tellor_abi.js
@@ -0,0 +1,46 @@
+export const TellorABI = [
+  {
+    constant: false,
+    inputs: [
+      {
+        name: '_to',
+        type: 'address'
+      },
+      {
+        name: '_value',
+        type: 'uint256'
+      }
+    ],
+    name: 'transfer',
+    outputs: [
+      {
+        name: '',
+        type: 'bool'
+      }
+    ],
+    payable: false,
+    stateMutability: 'nonpayable',
+    type: 'function'
+  },
+  {
+    constant: true,
+    inputs: [
+      {
+        name: '_owner',
+        type: 'address'
+      }
+    ],
+    name: 'balanceOf',
+    outputs: [
+      {
+        name: 'balance',
+        type: 'uint256'
+      }
+    ],
+    payable: false,
+    stateMutability: 'view',
+    type: 'function'
+  },
+];
+
+export const TELLORADDRESS = '0x0Ba45A8b5d5575935B8158a88C631E9F9C95a2e5';
